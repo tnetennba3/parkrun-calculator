14b215bb313567e86d4f791ac6b70a77a6f7593b
Helena Thompson <helenathompson.dev@gmail.com>
2025-09-12 15:18:07 +0100

    fix: Use responsive styles for filters
    On mobile, the date range picker was too large and therefore
    unusable.

--- src/components/results/Filters.tsx
+++ src/components/results/Filters.tsx
@@ -1,49 +1,64 @@
-import { Group, Select, SelectProps } from "@mantine/core";
+import { Group, Select } from "@mantine/core";
 import { DatePickerInput } from "@mantine/dates";
 import { DateTime } from "luxon";
 
 import { useResults } from "./context";
 
 import { parkruns } from "@/data/uk_parkrun_sss";
+import { Parkrun } from "@/types";
 
-export const Filters = () => {
-  const { dateRange, setDateRange, targetParkrun, setTargetParkrun } =
-    useResults();
+const DateRangePicker = ({ ...responsiveProps }) => {
+  const { dateRange, setDateRange } = useResults();
   const now = DateTime.now();
   const allTime: [string, string] = ["2004-10-02", now.toISO()];
 
   return (
-    <Group mb="lg">
-      <DatePickerInput
-        type="range"
-        label="Date range"
-        placeholder="Select date range"
-        value={dateRange}
-        onChange={setDateRange}
-        presets={[
-          {
-            value: allTime,
-            label: "All time",
-          },
-          {
-            value: [now.startOf("year").toISO(), now.endOf("year").toISO()],
-            label: "This year",
-          },
-          {
-            value: [now.minus({ months: 12 }).toISO(), now.toISO()],
-            label: "Last 12 months",
-          },
-        ]}
-      />
-      <Group>
-        <Select
-          searchable
-          label="As if run at"
-          data={parkruns}
-          value={targetParkrun}
-          onChange={setTargetParkrun as SelectProps["onChange"]}
-        />
-      </Group>
-    </Group>
+    <DatePickerInput
+      {...responsiveProps}
+      type="range"
+      label="Date range"
+      placeholder="Select date range"
+      value={dateRange}
+      onChange={setDateRange}
+      presets={[
+        {
+          value: allTime,
+          label: "All time",
+        },
+        {
+          value: [now.startOf("year").toISO(), now.endOf("year").toISO()],
+          label: "This year",
+        },
+        {
+          value: [now.minus({ months: 12 }).toISO(), now.toISO()],
+          label: "Last 12 months",
+        },
+      ]}
+    />
   );
 };
+
+const ParkrunPicker = ({ ...responsiveProps }) => {
+  const { targetParkrun, setTargetParkrun } = useResults();
+
+  return (
+    <Select
+      {...responsiveProps}
+      searchable
+      label="As if run at"
+      data={parkruns}
+      value={targetParkrun}
+      onChange={(parkrun) => parkrun && setTargetParkrun(parkrun as Parkrun)}
+    />
+  );
+};
+
+export const Filters = () => (
+  <Group mb="lg">
+    <DateRangePicker size="xs" hiddenFrom="xs" />
+    <DateRangePicker size="sm" visibleFrom="xs" />
+
+    <ParkrunPicker size="xs" hiddenFrom="xs" />
+    <ParkrunPicker size="sm" visibleFrom="xs" />
+  </Group>
+);
